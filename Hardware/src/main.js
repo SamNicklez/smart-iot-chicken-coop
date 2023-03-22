2f4b20385a0b3145206312384de5eb583a937c19
ColeArduser <ardusercole@gmail.com>
2023-03-21 19:39:43 -0500

    api shit

--- Hardware/src/main.js
+++ Hardware/src/main.js
@@ -1,18 +1,71 @@
-const coop = "coop1"
-const enter_date = "7:00"
-const enter_mass = 15
-const exit_date = "8:21"
-const exit_mass = 13
-
-chickenEntered(coop, enter_date, enter_mass)
-chickenExited(enter_date, exit_date, exit_mass)
+const fetch = require("node-fetch");
 
 
-async function chickenEntered(coop, enter_date, enter_mass) {
-    const fetch = require("node-fetch");
+testGetRecords();
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// CAMERA
+
+async function createCamera(id, box, coop) {
+    fetch("http://172.17.9.55:5000/api/camera", {
+        method: 'POST',
+        body: JSON.stringify({
+            id: id,
+            box: box,
+            coop: coop,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+    })
+}
+
+async function editCamera(id, box, coop) {
+    fetch("http://172.17.9.55:5000/api/camera", {
+        method: 'PATCH',
+        body: JSON.stringify({
+            id: id,
+            box: box,
+            coop: coop,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          }
+    })
+}
+
+async function getOneCamera(id) {
+    var response = await fetch(`http://172.17.9.55:5000/api/camera/${id}`, {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json"
+          }
+    })
+    .then((response) => response.json())
+    return response;
+}
+
+async function getCameras() {
+    var response = await fetch(`http://172.17.9.55:5000/api/camera`, {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json"
+          }
+    })
+    .then((response) => response.json())
+    return response;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// CHICKEN
+
+async function chickenEntered(box, coop, enter_date, enter_mass) {
     fetch("http://172.17.9.55:5000/api/chicken", {
         method: 'POST',
         body: JSON.stringify({
+            box: box,
             coop: coop,
             enter_date: enter_date,
             enter_mass: enter_mass,
@@ -24,7 +77,6 @@ async function chickenEntered(coop, enter_date, enter_mass) {
 }
 
 async function chickenExited(enter_date, exit_date, exit_mass) {
-    const fetch = require("node-fetch");
     fetch("http://172.17.9.55:5000/api/chicken", {
         method: 'PATCH',
         body: JSON.stringify({
@@ -36,5 +88,76 @@ async function chickenExited(enter_date, exit_date, exit_mass) {
             "Content-type": "application/json; charset=UTF-8"
           }
     })
+}
+
+async function getNumRecords(num) {
+    var response = await fetch(`http://172.17.9.55:5000/api/chicken/?numberOfRecords=${num}`, {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json"
+          }
+    })
+    .then((response) => response.json())
+    return response;
+}
+
+async function getRecords() {
+    var response = await fetch("http://172.17.9.55:5000/api/chicken", {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json"
+          }
+    })
+    .then((response) => response.json())
+    return response;
+}
+
+
+
+
+////////////////////////////////////////////////////////////
+// TESTS
+
+async function testChicken(){
+    var coop = "coop1"
+    var enter_date = "4:00"
+    var enter_mass = 15
+    var exit_date = "12:21"
+    var exit_mass = 13
+    
+    chickenEntered(coop, enter_date, enter_mass)
+    console.log("chicken entered")
+    chickenExited(enter_date, exit_date, exit_mass)
+    console.log("chicken exited")
+}
+
+async function testGetRecords() {
+    var resp = await getNumRecords(4);
+    console.log(resp);
+    var resp = await getRecords();
+    console.log(resp);
+}
+
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function makeChickens(num) {
+    for (let n = 0; n < num; n++) {
+        await chickenEntered(n, n, new Date(), n)
+        await chickenExited(n, n, n)
+        await delay(10000);
+    }
+}
+
+async function testCamera() {
+    await editCamera("CAM1", 1, "COOP1")
+    var resp = await getOneCamera("CAM1")
+    console.log(resp)
+    await editCamera("CAM1", 2, "COOP2")
+    var resp = await getOneCamera("CAM1")
+    console.log(resp)
+
+    var resp = await getCameras()
+    console.log(resp)
 }
 
--- Server/functions/index.js
+++ Server/functions/index.js
@@ -18,12 +18,95 @@ admin.initializeApp({
   databaseURL: "https://iotfinalproject-db88d-default-rtdb.firebaseio.com"});
 const db = admin.firestore();
 
+// CAMERA
 
-// log chicken entering coop
+// POST new camera
+app.post("/api/camera", jsonParser, (req, res) => {
+  (async () => {
+    try {
+      await db.collection("camera").doc("/" + req.body.id + "/").create({
+        box: req.body.box,
+        coop: req.body.coop,
+      });
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// PATCH camera's coop and box
+app.patch("/api/camera", jsonParser, (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("camera").doc(req.body.id);
+      await document.update({
+        box: req.body.box,
+        coop: req.body.coop,
+      });
+      return res.status(200).send();
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// GET specific camera info
+app.get("/api/camera/:camera_id", jsonParser, (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("camera").doc(req.params.camera_id);
+      const response = await query.get().then((querySnapshot) => {
+        const doc = querySnapshot;
+        const selectedItem = {
+          id: req.params.camera_id,
+          box: doc.data().box,
+          coop: doc.data().coop,
+        };
+        return selectedItem;
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// GET all camera info
+app.get("/api/camera", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("camera");
+      const response = [];
+      await query.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs;
+        for (const doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            box: doc.data().box,
+            coop: doc.data().coop,
+          };
+          response.push(selectedItem);
+        }
+        return response;
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// CHICKEN
+
+// POST chicken entering coop
 app.post("/api/chicken", jsonParser, (req, res) => {
   (async () => {
     try {
       await db.collection("chicken").doc("/" + req.body.enter_date + "/").create({
+        box: req.body.box,
         coop: req.body.coop,
         enter_date: req.body.enter_date,
         exit_date: null,
@@ -38,7 +121,7 @@ app.post("/api/chicken", jsonParser, (req, res) => {
 });
 
 
-// log chicken exiting coop
+// PATCH chicken exiting coop
 app.patch("/api/chicken", jsonParser, (req, res) => {
   (async () => {
     try {
@@ -54,90 +137,153 @@ app.patch("/api/chicken", jsonParser, (req, res) => {
   })();
 });
 
+// GET certain number of chicken data
+app.get("/api/chicken", (req, res) => {
+  if (req.query.numberOfRecords == undefined) {
+    (async () => {
+      try {
+        const query = db.collection("chicken").orderBy("enter_date", "desc");
+        const response = [];
+        await query.get().then((querySnapshot) => {
+          const docs = querySnapshot.docs;
+          for (const doc of docs) {
+            const selectedItem = {
+              box: doc.data().box,
+              coop: doc.data().coop,
+              enter_date: doc.data().enter_date,
+              enter_mass: doc.data().enter_mass,
+              exit_date: doc.data().exit_date,
+              exit_mass: doc.data().exit_mass,
+            };
+            response.push(selectedItem);
+          }
+          return response;
+        });
+        return res.status(200).send(response);
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+      }
+    })();
+  }
+  else {
+    (async () => {
+      try {
+        const query = db.collection("chicken").orderBy("enter_date", "desc").limit(parseInt(req.query.numberOfRecords));
+        const response = [];
+        await query.get().then((querySnapshot) => {
+          const docs = querySnapshot.docs;
+          for (const doc of docs) {
+            const selectedItem = {
+              box: doc.data().box,
+              coop: doc.data().coop,
+              enter_date: doc.data().enter_date,
+              enter_mass: doc.data().enter_mass,
+              exit_date: doc.data().exit_date,
+              exit_mass: doc.data().exit_mass,
+            };
+            response.push(selectedItem);
+          }
+          return response;
+        });
+        return res.status(200).send(response);
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
+      }
+    })();
+  }
+  });
 
 
-// // create
-// app.post("/api/create", (req, res) => {
-//   (async () => {
-//     try {
-//       await db.collection("items").doc("/" + req.body.id + "/").create({
-//         item: req.body.item});
-//       return res.status(200).send();
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
+// COOP
 
-// // read item
-// app.get("/api/read/:item_id", (req, res) => {
-//   (async () => {
-//     try {
-//       const document = db.collection("items").doc(req.params.item_id);
-//       const item = await document.get();
-//       const response = item.data();
-//       return res.status(200).send(response);
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
+// 
 
-// // read all
-// app.get("/api/read", (req, res) => {
-//   (async () => {
-//     try {
-//       const query = db.collection("items");
-//       const response = [];
-//       await query.get().then((querySnapshot) => {
-//         const docs = querySnapshot.docs;
-//         for (const doc of docs) {
-//           const selectedItem = {
-//             id: doc.id,
-//             item: doc.data().item,
-//           };
-//           response.push(selectedItem);
-//         }
-//         return response;
-//       });
-//       return res.status(200).send(response);
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
 
-// // update
-// app.put("/api/update/:item_id", (req, res) => {
-//   (async () => {
-//     try {
-//       const document = db.collection("items").doc(req.params.item_id);
-//       await document.update({
-//         item: req.body.item,
-//       });
-//       return res.status(200).send();
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
 
-// // delete
-// app.delete("/api/delete/:item_id", (req, res) => {
-//   (async () => {
-//     try {
-//       const document = db.collection("items").doc(req.params.item_id);
-//       await document.delete();
-//       return res.status(200).send();
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
+// create
+app.post("/api/create", (req, res) => {
+  (async () => {
+    try {
+      await db.collection("items").doc("/" + req.body.id + "/").create({
+        item: req.body.item});
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// read item
+app.get("/api/read/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      const item = await document.get();
+      const response = item.data();
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// read all
+app.get("/api/read", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("items");
+      const response = [];
+      await query.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs;
+        for (const doc of docs) {
+          const selectedItem = {
+            id: doc.id,
+            item: doc.data().item,
+          };
+          response.push(selectedItem);
+        }
+        return response;
+      });
+      return res.status(200).send(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// update
+app.put("/api/update/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      await document.update({
+        item: req.body.item,
+      });
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
+
+// delete
+app.delete("/api/delete/:item_id", (req, res) => {
+  (async () => {
+    try {
+      const document = db.collection("items").doc(req.params.item_id);
+      await document.delete();
+      return res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send(error);
+    }
+  })();
+});
 
 exports.app = functions.https.onRequest(app);
